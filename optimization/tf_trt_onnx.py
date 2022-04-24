91b3a2a21ea724436547a13378ef600cb413edd3
minhquan186 <lmquan.1860@gmail.com>
2022-04-24 09:33:28 +0700

    Update

--- optimization/tf_trt_onnx.py
+++ optimization/tf_trt_onnx.py
@@ -1,3 +1,25 @@
 #!/mnt/f/MICA/.endo_env/bin/python3
 
+import argparse
+import onnx
+import subprocess
 import tf2onnx
+
+parser = argparse.ArgumentParser()
+parser.add_argument('saved_model', type=str, help='Tensorflow 2.0 Saved model path')
+parser.add_argument('onnx', type=str, help='Path to save onnx model')
+parser.add_argument('save_trt', type=str, help='TensorRT engine output path')
+
+args = parser.parse_args()
+
+input_model_dir = args.saved_model
+onnx_path = args.onnx
+
+subprocess.run(["python","-m","tf2onnx.convert","--opset","11","--saved-model",input_model_dir, "--output", "tmp.onnx"], check=True)
+
+onnx_model = onnx.load('tmp.onnx')
+onnx_model.graph.input[0].type.tensor_type.shape.dim[0].dim_param = '?'
+onnx.save(onnx_model, onnx_path)
+onnx.checker.check_model(onnx_model)
+
+subprocess.run(["rm", "-fv", "tmp.onnx"])
--- optimization/tfrt_quant.py
+++ optimization/tfrt_quant.py
@@ -1,9 +1,10 @@
 #!/mnt/f/MICA/.endo_env/bin/python3
 
 import argparse
-import tensorflow as tf
+# import tensorflow as tf
+import os
+from tensorflow.python.compiler.tensorrt import trt_convert as trt
 
-print(f'Imported necessary library!')
 parser = argparse.ArgumentParser()
 parser.add_argument('tf2',    type=str, help='TF2.0 Saved Model dir')
 parser.add_argument('prec',   type=str, help='Precision type (FP32/FP16/INT8)')
@@ -12,22 +13,37 @@ args = parser.parse_args()
 
 input_model_dir = args.tf2
 print(f'Saved Model loaded!')
-FP = args.prec
+precision = args.prec
 output_saved_model_dir = args.output
 
-params = tf.experimental.tensorrt.ConversionParams(
-    precision_mode=FP)
-converter = tf.experimental.tensorrt.Converter(
-                                input_saved_model_dir=input_model_dir,
-                                conversion_params=params
-                                )
-converter.convert(is_dynamic_op=True)
+if not os.path.exists(output_saved_model_dir):
+    os.makedirs(output_saved_model_dir)
 
-print(f"Converting to TensorRT...")
+# params = tf.experimental.tensorrt.ConversionParams(
+#     precision_mode=FP)
+# converter = tf.experimental.tensorrt.Converter(
+#                                 input_saved_model_dir=input_model_dir,
+#                                 conversion_params=params
+#                                 )
+# converter.convert(is_dynamic_op=True)
+
+# print(f"Converting to TensorRT...")
+# converter.convert()
+
+conversion_params = trt.DEFAULT_TRT_CONVERSION_PARAMS._replace(precision_mode=precision, 
+                                                                    max_workspace_size_bytes=8000000000,
+                                                                    use_calibration= precision == "INT8")
+
+converter = trt.TrtGraphConverterV2(input_saved_model_dir=input_model_dir,
+                                    conversion_params=conversion_params)
+
+if precision == "INT8":
+    converter.convert(calibration_input_fn=calibration_data)
+else:
+    converter.convert()
 converter.convert()
-
-# converter.build(input_fn=representative_dataset_gen)
 
 # Save the TRT engine and the engines.
 converter.save(output_saved_model_dir)
+
 print(f'TensorRT Model successfully converted and saved at {output_saved_model_dir}')
